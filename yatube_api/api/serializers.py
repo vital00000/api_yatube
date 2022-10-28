23ce24456c27e77100bc12eb3f78e0b9cffe6b59
Виталий Станкевич <vitaliy.v.stankevich>
2022-10-28 06:37:27 +0300

    поправил

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from posts.models import Comment, Group, Post
 
 
-
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username',
@@ -13,6 +12,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
 
 class GroupSerializer(serializers.ModelSerializer):
 
